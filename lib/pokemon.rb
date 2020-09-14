213ca173883370a9fa4fa308e0276a4f4d640db3
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-14 00:39:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,11 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
 
-
+  def self.find(id, db)
+    
+    db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
+    
+  end
 
 
 
